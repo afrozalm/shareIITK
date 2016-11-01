8362767719453a5ad6f6b8e5e6f949ef54813a38
srishti0610 <srishtij@iitk.ac.in>
2016-11-02 00:41:51 +0530

    login,welcome,signup working fine

--- config/passport.js
+++ config/passport.js
@@ -83,7 +83,7 @@ module.exports = function(passport) {
         // by default, local strategy uses username and password, we will override with email
         usernameField : 'email',
         passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
+		passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) { // callback with email and password from our form
 
