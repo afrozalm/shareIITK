2ecb506d559cdf861fc949e6fe0211ffd4d5b134
afrozalm <alam.afroz011@gmail.com>
2016-11-02 11:30:43 +0530

    resolved passport error; need to fix mongoose

--- server.js
+++ server.js
@@ -8,22 +8,25 @@ const flash = require('connect-flash');
 
 app.set('view engine', 'ejs')
 
-var MongoClient = require('mongodb').MongoClient
-  , Server = require('mongodb').Server;
+//var MongoClient = require('mongodb').MongoClient
+  //, Server = require('mongodb').Server;
 
 var assert = require('assert');
 var morgan = require('morgan');
-var db
-var url = 'mongodb://localhost:3000/UserItem';
-var mongoClient = new MongoClient(new Server('localhost', 27017));
 
-mongoClient.open(function(err, mongoClient) {
-    if(err) {
-            console.log(err);
-    }
+require('./config/passport')(passport);
 
-    var userDB = mongoClient.db("users");
-    var itemsDB = mongoClient.db("items");
+//var db
+//var url = 'mongodb://localhost:3000/UserItem';
+//var mongoClient = new MongoClient(new Server('localhost', 27017));
+
+//mongoClient.open(function(err, mongoClient) {
+    //if(err) {
+            //console.log(err);
+    //}
+
+    //var userDB = mongoClient.db("users");
+    //var itemsDB = mongoClient.db("items");
 
     app.listen(3001, function(){
         console.log('listening on 3001')
@@ -34,7 +37,11 @@ mongoClient.open(function(err, mongoClient) {
     app.use(morgan('dev')); // log every request to the console
     app.use(cookieParser()); // read cookies (needed for auth)
     // required for passport
-    app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+    app.use(session({ secret: 'ilovescotchscotchyscotchscotch',
+                      resave: true,
+                      saveUninitialized: true
+    })); // session secret
+
     app.use(passport.initialize());
     app.use(passport.session()); // persistent login sessions
     app.use(flash()); //
@@ -45,4 +52,4 @@ mongoClient.open(function(err, mongoClient) {
                       saveUninitialized: false}));
 
     require('./app/routes.js')(app, passport); 
-});
+//});
