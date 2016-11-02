511eb80cb5f316f7671dd73209fc2267ae0671f5
afrozalm <alam.afroz011@gmail.com>
2016-11-02 12:00:22 +0530

    removed unnecessary comments

--- server.js
+++ server.js
@@ -9,50 +9,34 @@ const flash = require('connect-flash');
 
 app.set('view engine', 'ejs')
 
-//var MongoClient = require('mongodb').MongoClient
-  //, Server = require('mongodb').Server;
-
 var assert = require('assert');
 var morgan = require('morgan');
 var configDB = require('./config/database.js');
 
 mongoose.connect(configDB.url);
 
-//var db
-//var url = 'mongodb://localhost:3000/UserItem';
-//var mongoClient = new MongoClient(new Server('localhost', 27017));
+app.listen(3001, function(){
+    console.log('listening on 3001')
+})
 
-//mongoClient.open(function(err, mongoClient) {
-    //if(err) {
-            //console.log(err);
-    //}
+app.use(bodyParser.urlencoded({extended: true}))
 
-    //var userDB = mongoClient.db("users");
-    //var itemsDB = mongoClient.db("items");
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch',
+                  resave: true,
+                  saveUninitialized: true
+})); // session secret
 
-    app.listen(3001, function(){
-        console.log('listening on 3001')
-    })
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); //
 
-    app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({ cookie: { maxAge: 60000 }, 
+                  secret: 'woot',
+                  resave: false, 
+                  saveUninitialized: false}));
 
-    app.use(morgan('dev')); // log every request to the console
-    app.use(cookieParser()); // read cookies (needed for auth)
-    // required for passport
-    app.use(session({ secret: 'ilovescotchscotchyscotchscotch',
-                      resave: true,
-                      saveUninitialized: true
-    })); // session secret
-
-    app.use(passport.initialize());
-    app.use(passport.session()); // persistent login sessions
-    app.use(flash()); //
-
-    app.use(session({ cookie: { maxAge: 60000 }, 
-                      secret: 'woot',
-                      resave: false, 
-                      saveUninitialized: false}));
-
-    require('./config/passport')(passport);
-    require('./app/routes.js')(app, passport); 
-//});
+require('./config/passport')(passport);
+require('./app/routes.js')(app, passport); 
