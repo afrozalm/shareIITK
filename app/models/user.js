e24dc968cc6cd19eebb2310b66b231a2a35f0273
srishti0610 <srishtij@iitk.ac.in>
2016-11-09 11:27:35 +0530

    Categiry based schema updated

--- app/models/user.js
+++ app/models/user.js
@@ -8,15 +8,16 @@ var itemSchema = new Schema({
 
     name        : { type: String, required: true  },                                    // item name
     username    : { type : mongoose.Schema.Types.ObjectId, ref: userSchema  },          // owner name
-    category    : { type: String, required: true  },                                    // category
+    //category    : { type: String, required: true  },                                    // category
     request_notification : { type : mongoose.Schema.Types.ObjectId, default: null },    // id of user which requested this item
 
 	item_status : Number, // 0 for delete, 1 for invisible, 2 for visible
-	//category    : {
-        //book         : Boolean,
-        //cycle        : Boolean,
-        //mattress     : Boolean,
-    //},
+	category    : {
+		book and study material       : Boolean,
+		cycle        : Boolean,
+		mattress     : Boolean,
+		//cooler		 : Boolean,	
+	},
     description : String
 });
 
